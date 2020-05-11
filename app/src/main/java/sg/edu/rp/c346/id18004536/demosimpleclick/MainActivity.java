df0e69c2f5e1c46885ad6a86b2d70a46be59868e
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 11:05:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18004536/demosimpleclick/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id18004536.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText eInput;
+    ToggleButton tbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        eInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code for the action
+                eInput.getText().toString();
+                String stringResponse = eInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+            }
+        });
+
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnDisplay.setEnabled(false);
+            }
+        });
+    }
+}
